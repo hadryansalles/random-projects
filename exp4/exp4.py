218afb2f0101e158c8937955fde20999bba3c202
Hadryan <37905502+hadryans@users.noreply.github.com>
2019-11-25 00:20:41 -0300

    Add files via upload

--- exp4/exp4.py
+++ exp4/exp4.py
@@ -15,10 +15,17 @@ def f_r2(c1, c2):
 def round_sig(x, sig=1):
   return round(x, sig-int(floor(log10(abs(x))))-1)
 
-for i in range(-3, -7, -1):
+
+
+for i in range(-3, -8, -1):
   for j in range(9, 0, -1):
-    for k in range(9, 0, -2):
+    for k in range(9, -1, -1):
       c1 = (j+k/10)*10**i
       c2 = f_c2(c1)
       r2 = f_r2(c1, c2)
-      print(round_sig(c1, 2), round_sig(c2, 2), round(r2))
+      print("C1:", round(round_sig(c1, 4)/10**-9), "nF", "\tC2:", round(round_sig(c2, 4)/10**-9), "nF", "\tR2:", round(r2), "Ohms")
+
+# c1 = 4.7*10**-6
+# c2 = f_c2(c1)
+# r2 = f_r2(c1, c2)
+# print(round_sig(c1, 2), round_sig(c2, 3), round(r2))
